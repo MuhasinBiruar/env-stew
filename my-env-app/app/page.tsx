f6e9f75a064e672577624cc0af3074827b99dc33
MuhasnBiruare <mxebiruar@addu.edu.ph>
2026-02-23 13:22:09 +0800

    yea

--- my-env-app/app/page.tsx
+++ my-env-app/app/page.tsx
@@ -1,11 +1,18 @@
-import Image from "next/image";
+import { Image } from 'primereact/image';
+
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        
-      </main>
+    <>
+    <div className="flex min-h-screen items-center justify-center font-sans dark:bg-black" style={{background: 'linear-gradient(to right, #31694E, #658C58, #BBC863, #F0E491)'}}>
+      <h1>Hello</h1>
+        <div>
+        <Image src="/debi_tirar_mas_fotos/IMG_20260222_150949.jpg" alt="Image" width="400" 
+        className="self-center hover:brightness-75 transition-all duration-300 cursor-pointer drop-shadow-white drop-shadow-xl" 
+        imageClassName="rounded-lg border-2 border-white" 
+        preview />
+        </div>
     </div>
+    </>
   );
 }
